cdfb19812b1aedc57b4771998b18dad4778194bc
matthewgreene <greene.matthew@northeastern.edu>
2023-07-24 15:36:30 -0400

    abstracted the generation method

--- number_games/count.d
+++ number_games/count.d
@@ -1,23 +1,37 @@
 //start of d file
-
-void main() {
 import std.file;
 import std.stdio;
 import std.path;
 import std.conv;
+
 int[10] fib;
-int a = 0;
-for (int i=0; i < 10; i++) {
-    if (i > 0){
-        fib[i] = fib[i-1] + a;
-        a = fib[i-1];
-    } else {
-        fib[i] = a + 1;
-        a = fib[i];
-    }
-    // a = fib[i]; 
-    writeln("fib value: " ~ to!string(fib[i]));
+int[] getArray() {
+
+    int a = 0;
+    for (int i=0; i < 10; i++) {
+        if (i > 0){
+            fib[i] = fib[i-1] + a;
+            a = fib[i-1];
+        } else {
+            fib[i] = a + 1;
+            a = fib[i];
+        }
+        // a = fib[i]; 
+        
+        }
+    return fib;
 }
+
+
+void main() {
+
+    auto arr = getArray();
+    foreach (e; 0..10) {
+        writeln("fib value: " ~ to!string(arr[e]));
+    }
+
+
+
 // writeln("hello world, we are going to read from file");
 // string fibPath = relativePath("../fib.txt");
 // auto fib = read(fibPath);
